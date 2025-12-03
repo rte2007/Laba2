e09f33967db273359d83f8f754dcc6348c4408d6
rte2007 <154438948+rte2007@users.noreply.github.com>
2025-12-03 18:45:37 +0300

    Update Sunkern.java

--- src/pokemons/Sunkern.java
+++ src/pokemons/Sunkern.java
@@ -1,4 +1,25 @@
 package pokemons;
 
-public class Sunkern {
+
+import moves.sunflora.BulletSeed;
+import moves.sunflora.DoubleTeam;
+import moves.sunflora.SludgeBomb;
+import moves.sunflora.SwordsDance;
+import ru.ifmo.se.pokemon.Pokemon;
+import ru.ifmo.se.pokemon.Type;
+
+public final class Sunkern extends Pokemon{
+
+    public Sunkern(String name, int level) {
+        super(name, level);
+
+        super.setType(Type.GRASS);
+        super.setStats(30, 30, 30, 30, 30, 30);
+
+        SludgeBomb sludgeBomb = new SludgeBomb(90, 100);
+        SwordsDance swordsDance = new SwordsDance(0, 0);
+        DoubleTeam doubleTeam = new DoubleTeam(0, 0);
+
+        super.setMove(sludgeBomb, swordsDance, doubleTeam);
+    }
 }
