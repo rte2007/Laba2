65a25847c6f1f950d552d15edfc41a1bd8599876
rte2007 <154438948+rte2007@users.noreply.github.com>
2025-12-03 18:42:34 +0300

    Update DoubleTeam.java

--- src/moves/sunflora/DoubleTeam.java
+++ src/moves/sunflora/DoubleTeam.java
@@ -1,4 +1,25 @@
 package moves.sunflora;
 
-public class DoubleTeam {
+import ru.ifmo.se.pokemon.*;
+
+public final class DoubleTeam extends StatusMove {
+    public DoubleTeam(double pow, double acc) {
+        super(Type.NORMAL, pow, acc);
+    }
+
+    @Override
+    protected void applySelfEffects(Pokemon p) {
+        super.applySelfEffects(p);
+
+        Effect e = new Effect().stat(Stat.EVASION, 1);
+        p.addEffect(e);
+
+    }
+
+    @Override
+    protected String describe() {
+        String[] pieces = this.getClass().toString().split("\\.");
+
+        return "does " + pieces[pieces.length - 1];
+    }
 }
