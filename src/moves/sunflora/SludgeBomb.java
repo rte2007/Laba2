aaa1460efd1b53988f34222b71208d055fa95c55
rte2007 <154438948+rte2007@users.noreply.github.com>
2025-12-03 18:42:48 +0300

    Update SludgeBomb.java

--- src/moves/sunflora/SludgeBomb.java
+++ src/moves/sunflora/SludgeBomb.java
@@ -1,4 +1,30 @@
 package moves.sunflora;
 
-public class SludgeBomb {
+import ru.ifmo.se.pokemon.*;
+
+public final class SludgeBomb extends SpecialMove {
+    public SludgeBomb(double pow, double acc) {
+        super(Type.POISON, pow, acc);
+    }
+
+    @Override
+    protected void applyOppEffects(Pokemon p) {
+        super.applyOppEffects(p);
+        if(Lab2.Program.chance(0.3)) {
+            switch (p.getTypes().toString()) {
+                case "[POISON]", "[STEEL]" -> {
+                }
+                default -> Effect.poison(p);
+            }
+        }
+    }
+
+    @Override
+    protected String describe() {
+        String[] pieces = this.getClass().toString().split("\\.");
+
+        return "does " + pieces[pieces.length-1];
+    }
+
+
 }
