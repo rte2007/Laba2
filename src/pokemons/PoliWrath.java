4f9d974d9e2645b7b13e1c777e7177bedc7ff500
rte2007 <154438948+rte2007@users.noreply.github.com>
2025-12-03 18:43:40 +0300

    Update PoliWrath.java

--- src/pokemons/PoliWrath.java
+++ src/pokemons/PoliWrath.java
@@ -1,4 +1,20 @@
 package pokemons;
 
-public class PoliWrath {
+import moves.poliwrath.*;
+import ru.ifmo.se.pokemon.Pokemon;
+import ru.ifmo.se.pokemon.Type;
+
+public final class Poliwag extends Pokemon{
+
+    public Poliwag(String name, int level) {
+        super(name, level);
+
+        super.setType(Type.WATER);
+        super.setStats(40, 50, 40, 40, 40, 90);
+
+        IceBeam iceBeam = new IceBeam(90, 100);
+        BubbleBeam bubbleBeam = new BubbleBeam(65, 100);
+
+        super.setMove(iceBeam, bubbleBeam);
+    }
 }
