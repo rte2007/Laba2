cae09978cfcd78c3f014657046245ed001e2bd9f
rte2007 <154438948+rte2007@users.noreply.github.com>
2025-12-03 18:43:55 +0300

    Update Poliwag.java

--- src/pokemons/Poliwag.java
+++ src/pokemons/Poliwag.java
@@ -1,4 +1,21 @@
 package pokemons;
 
-public class Poliwag {
+import moves.poliwrath.*;
+import ru.ifmo.se.pokemon.Pokemon;
+import ru.ifmo.se.pokemon.Type;
+
+public final class Poliwhirl extends Pokemon{
+
+    public Poliwhirl(String name, int level) {
+        super(name, level);
+
+        super.setType(Type.WATER);
+        super.setStats(65, 65, 65, 50, 50, 90);
+
+        IceBeam iceBeam = new IceBeam(90, 100);
+        BubbleBeam bubbleBeam = new BubbleBeam(65, 100);
+        HydroPump hydroPump = new HydroPump(110, 80);
+
+        super.setMove(iceBeam, bubbleBeam, hydroPump);
+    }
 }
