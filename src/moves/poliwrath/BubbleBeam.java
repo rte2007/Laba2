13077a8492abbff90faafe32b6e6cb3b8613dc5f
rte2007 <154438948+rte2007@users.noreply.github.com>
2025-12-03 18:40:48 +0300

    Update BubbleBeam.java

--- src/moves/poliwrath/BubbleBeam.java
+++ src/moves/poliwrath/BubbleBeam.java
@@ -1,4 +1,30 @@
 package moves.poliwrath;
 
-public class BubbleBeam {
+import ru.ifmo.se.pokemon.*;
+
+public final class BubbleBeam extends SpecialMove {
+
+    public BubbleBeam(double pow, double acc) {
+        super(Type.WATER, pow, acc);
+
+    }
+
+    @Override
+    protected void applyOppEffects(Pokemon p) {
+        super.applyOppEffects(p);
+
+        if(Lab2.Program.chance(0.1)) {
+            Effect e = new Effect().stat(Stat.SPEED, -1);
+
+            p.addEffect(e);
+        }
+
+    }
+
+    @Override
+    protected String describe() {
+        String[] pieces = this.getClass().toString().split("\\.");
+
+        return "does " + pieces[pieces.length-1];
+    }
 }
