1309f67949234a97b11cc32c1987627e23744374
rte2007 <154438948+rte2007@users.noreply.github.com>
2025-12-03 18:41:36 +0300

    Update IceBeam.java

--- src/moves/poliwrath/IceBeam.java
+++ src/moves/poliwrath/IceBeam.java
@@ -1,4 +1,28 @@
 package moves.poliwrath;
 
-public class IceBeam {
+import ru.ifmo.se.pokemon.*;
+
+public final class IceBeam extends SpecialMove {
+
+    public IceBeam(double pow, double acc) {
+        super(Type.ICE, pow, acc);
+
+    }
+
+    @Override
+    protected void applyOppEffects(Pokemon p) {
+        super.applyOppEffects(p);
+
+        if(Lab2.Program.chance(0.1)) {
+            Effect.freeze(p);
+        }
+
+    }
+
+    @Override
+    protected String describe() {
+        String[] pieces = this.getClass().toString().split("\\.");
+
+        return "does " + pieces[pieces.length-1];
+    }
 }
