ed19ba9e3c7e45081d001f0653c9a03764a53898
rte2007 <154438948+rte2007@users.noreply.github.com>
2025-12-03 18:43:03 +0300

    Update SwordsDance.java

--- src/moves/sunflora/SwordsDance.java
+++ src/moves/sunflora/SwordsDance.java
@@ -1,4 +1,25 @@
 package moves.sunflora;
 
-public class SwordsDance {
+import ru.ifmo.se.pokemon.*;
+
+public final class SwordsDance extends StatusMove {
+    public SwordsDance(double pow, double acc) {
+        super(Type.NORMAL, pow, acc);
+    }
+
+    @Override
+    protected void applySelfEffects(Pokemon p) {
+        super.applySelfEffects(p);
+
+        Effect e = new Effect().stat(Stat.ATTACK, 2);
+        p.addEffect(e);
+
+    }
+
+    @Override
+    protected String describe() {
+        String[] pieces = this.getClass().toString().split("\\.");
+
+        return "does " + pieces[pieces.length - 1];
+    }
 }
