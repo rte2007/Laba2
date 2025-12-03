425636de38b7e7e7bf0911e655cb4c9e556bd43e
rte2007 <154438948+rte2007@users.noreply.github.com>
2025-12-03 18:39:42 +0300

    Update CrushClaw.java

--- src/moves/druddigon/CrushClaw.java
+++ src/moves/druddigon/CrushClaw.java
@@ -1,4 +1,27 @@
 package moves.druddigon;
 
-public class CrushClaw {
+import ru.ifmo.se.pokemon.*;
+
+public final class CrushClaw extends PhysicalMove {
+
+    public CrushClaw(double pow, double acc) {
+        super(Type.NORMAL, pow, acc);
+
+    }
+
+    @Override
+    protected void applyOppEffects(Pokemon p) {
+        super.applyOppEffects(p);
+
+        Effect e = new Effect().chance(0.5).stat(Stat.DEFENSE, -1);
+
+        p.addEffect(e);
+    }
+
+    @Override
+    protected String describe() {
+        String[] pieces = this.getClass().toString().split("\\.");
+
+        return "does " + pieces[pieces.length-1];
+    }
 }
