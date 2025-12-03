daa9017a1fa68a01d5b3f478f09aac0a4a68de1a
rte2007 <154438948+rte2007@users.noreply.github.com>
2025-12-03 18:41:19 +0300

    Update Hypnosis.java

--- src/moves/poliwrath/Hypnosis.java
+++ src/moves/poliwrath/Hypnosis.java
@@ -1,4 +1,26 @@
 package moves.poliwrath;
 
-public class Hypnosis {
+import ru.ifmo.se.pokemon.*;
+
+public final class Hypnosis extends StatusMove {
+
+    public Hypnosis(double pow, double acc) {
+        super(Type.PSYCHIC, pow, acc);
+
+    }
+
+    @Override
+    protected void applyOppEffects(Pokemon p) {
+        // случайная длительность сна 1–3 хода
+        int turns = 1 + (int) (Math.random() * 3); // 1, 2 или 3
+
+        Effect sleep = new Effect().condition(Status.SLEEP).turns(turns);
+
+        p.setCondition(sleep);
+    }
+
+    @Override
+    protected String describe() {
+        return "использует Hypnosis и усыпляет противника";
+    }
 }
