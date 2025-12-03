923072218b947972f3e23d2288806cc146993bf6
rte2007 <154438948+rte2007@users.noreply.github.com>
2025-12-03 18:40:29 +0300

    Update Rest.java

--- src/moves/druddigon/Rest.java
+++ src/moves/druddigon/Rest.java
@@ -1,4 +1,31 @@
 package moves.druddigon;
 
-public class Rest {
+import ru.ifmo.se.pokemon.*;
+
+import static ru.ifmo.se.pokemon.Stat.HP;
+
+public final class Rest extends StatusMove {
+
+    public Rest(double pow, double acc) {
+        super(Type.PSYCHIC, pow, acc);
+
+    }
+
+    @Override
+    protected void applySelfEffects(Pokemon p) {
+        super.applySelfEffects(p);
+
+        p.setMod(Stat.HP, (int)(p.getHP() - p.getStat(Stat.HP))); // full heal (now - max)
+
+        Effect e = new Effect().condition(Status.SLEEP).turns(2);     // сон строго на 2 хода
+        p.addEffect(e);
+
+    }
+
+    @Override
+    protected String describe() {
+        String[] pieces = this.getClass().toString().split("\\.");
+
+        return "does " + pieces[pieces.length-1];
+    }
 }
